7f6304f5252adeb6e4d9ec95d889d0c1f8bf4e44
kalim <kalimies1234@gmail.com>
2021-09-03 16:35:25 +0530

    first  commit

---
+++ book/admin.py
@@ -0,0 +1,10 @@
+
+
+# Register your models here.
+from django.contrib import admin
+from .models import *
+ 
+# Register your models here.
+admin.site.register(Book)
+admin.site.register(Cart)
+admin.site.register(Customer)
